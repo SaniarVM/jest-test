b6da736d54941a1cba72e63192fbbf9ba4fd5d45
Saniar EDUARDO <saniar.eduardo@aifrance.com>
2022-11-06 01:50:39 +0100

    ssl disable

--- run.sh
+++ run.sh
@@ -9,3 +9,4 @@ export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
 export DRONE_USER_CREATE="username:${DRONE_GITHUB_ADMIN},machine:false,admin:true,token:${DRONE_RPC_SECRET}"
 docker-compose up -d
+
